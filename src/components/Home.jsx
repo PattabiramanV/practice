54f1143bcc856a99a19c2ee25e23fea303a3216c
PattabiramanV <pattabiramanvdckap@gmail.com>
2024-07-10 10:19:24 +0530

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
 import Section from "./Home/Section";
-import Header from "./Header_and_Footer/Header";
+import Header from "./headerandfooter/Header";
 import Petcard from "./Home/pet card/PetList";
 import Section1 from "./Home/Section1";
-import Footer from "./Header_and_Footer/Footer";
+import Footer from "./headerandfooter/Footer";
 // import Login from "./Login";
 
 function Home() {
--- src/components/PetHostel.jsx
+++ src/components/PetHostel.jsx
@@ -1,7 +1,7 @@
 // import Form from "antd/es/form/Form";
 import React from "react";
-import Footer from "../components/Header_and_Footer/Footer";
-import Header from "../components/Header_and_Footer/Header";
+import Footer from "./headerandfooter/Footer";
+import Header from "./headerandfooter/Header";
 import PetaddForm from "./PetHostel/AddPetForHos";
 import Hostels from "./PetHostel/Hostels"; // Adjusted the import path
 
